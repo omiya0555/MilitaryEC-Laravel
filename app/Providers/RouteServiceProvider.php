0debe042ddc4d36127b5c09f64d912d076c628f4
otoharadak@gmail.com <otoharadak@gmail.com>
2024-09-15 12:42:09 +0900

    Fix route of HOME
    HOME = /products/index -> /products

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/products/index';
+    public const HOME = '/products';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
