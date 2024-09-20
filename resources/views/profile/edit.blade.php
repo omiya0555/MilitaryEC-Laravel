06d7976a1438d7928994d952310b5be7ef699556
otoharadak@gmail.com <otoharadak@gmail.com>
2024-09-20 23:00:47 +0900

    Change the layout of profile pages.
    Add flex justify-center classes.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,8 +3,8 @@
     <p class="flex justify-center text-gray-700 mt-12 p-5">P R O F I L E</p>
     
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="flex justify-center p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
@@ -14,7 +14,7 @@
             
             <p class="flex justify-center text-gray-700 mt-12 p-5">P A S S W O R D</p>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="flex justify-center p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
@@ -24,7 +24,7 @@
             
             <p class="flex justify-center text-gray-700 mt-12 p-5">A C C O U N T</p>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="flex justify-center p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
